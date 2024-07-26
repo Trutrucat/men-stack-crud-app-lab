33c510639ca307ef4cb652dced40ab51cf1d181e
Trutrucat <167733097+Trutrucat@users.noreply.github.com>
2024-07-25 18:53:03 -0700

    changes

--- models/cat.js
+++ models/cat.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const CatSchema = new mongoose.Schema({
+const catSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   image: String,
--- server.js
+++ server.js
@@ -1,4 +1,28 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const Cat = require('.models/cat.js')
+const Cat = require('./models/cat.js')
+const MONGO_URI = process.env.MONGO_URI
+
+mongoose.connect(MONGO_URI)
+app.use(express.json())
+
+mongoose.connection.once('open', () => {
+    console.log('Conncection with Mongo is established')
+})
+
+mongoose.connection.on('error', () => {
+    console.log('MongoDb trippin')
+})
+
+app.get('/cats', async (req, res) => {
+    try{
+        const foundCats = await Cat.find({})
+        res.json(foundCats)
+    }catch (error) {
+        console.error(error)
+        res.status(400).json({ message: error.message })
+
+    }
+})
